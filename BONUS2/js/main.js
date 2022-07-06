d2b7f9b64086d92bd2d74443eb844f46773cac32
Simone Fortunato <frtsmn94@gmail.com>
2022-07-06 16:16:35 +0200

    bonus 2 Inserimento numeri blocchi con prompt

---
+++ BONUS2/js/main.js
@@ -0,0 +1,36 @@
+const NUMERO = parseInt(prompt("INSERISCI NUMERO DI BLOCCHI"));
+const container = document.getElementById("container");
+
+
+for ( let i = 1; i <= NUMERO; i++) {
+
+    if(i % 5 == 0 && i % 3 == 0 ){
+
+        const box = document.createElement("div");
+        box.append("FizzBuzz");
+        box.className = "fizzbuzz";
+        container.append(box);
+
+    } else if ( i % 3 == 0 ){
+
+        const box = document.createElement("div");
+        box.append("Fizz");
+        box.className = "fizz";
+        container.append(box);
+
+
+    }else if ( i % 5 == 0 ){
+
+        const box = document.createElement("div");
+        box.append("Buzz");
+        box.className = "buzz";
+        container.append(box);
+
+    }else{
+
+        const box = document.createElement("div");
+        box.append(i);
+        box.className = "box";
+        container.append(box);   
+     }
+}
